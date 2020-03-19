b1cb36b8798e6d0a7b820c1c612424cdb7802a4f
aamir miyajiwala <40755205+aamirmiy@users.noreply.github.com>
2020-03-19 10:48:08 +0530

    Add files via upload

---
+++ tic-tac-toe.py
@@ -0,0 +1,104 @@
+#tic tac toe game
+import random
+def display_board(board):
+    print('   |   |   ')
+    print(' '+board[7]+' | '+board[8]+' | '+board[9])
+    print('   |   |   ')
+    print('--------------')
+    print('   |   |   ')
+    print(' '+board[4]+' | '+board[5]+' | '+board[6])
+    print('   |   |   ')
+    print('--------------')
+    print('   |   |   ')
+    print(' '+board[1]+' | '+board[2]+' | '+board[3])
+    print('   |   |   ')
+def player_input():
+    marker=' '
+    while not (marker=='X' or marker=='O'):
+        marker=input('Player 1: Do you want to be X or O?').upper()
+    if marker == 'X':
+        return('X','O')
+    else:
+        return('O','X')
+def place_marker(board,marker,position):
+    board[position]=marker
+def win_check(board,mark):
+    return((board[7] == mark and board[8] == mark and board[9] == mark) or
+           (board[7] == mark and board[6] == mark and board[3] == mark) or
+           (board[7] == mark and board[4] == mark and board[1] == mark) or
+           (board[8] == mark and board[5] == mark and board[2] == mark) or
+           (board[9] == mark and board[6] == mark and board[3] == mark) or
+           (board[9] == mark and board[5] == mark and board[1] == mark) or
+           (board[1] == mark and board[2] == mark and board[3] == mark) or
+           (board[4] == mark and board[5] == mark and board[6] == mark))
+def choose_first():
+    if random.randint(0,1) == 0:
+        return 'Player 2'
+    else:
+        return 'Player 1'
+def space_check(board,position):
+    return board[position]==' '
+def full_board_check(board):
+    for i in range(1,10):
+        if space_check(board,i):
+            return False
+    return True
+def player_choice(board):
+    # Using strings because of raw_input
+    position = ' '
+    while position not in '1 2 3 4 5 6 7 8 9'.split() or not space_check(board, int(position)):   
+        position = input('Choose your next position: (1-9) ')
+    return int(position)
+def replay():
+    return input('Do you want play again? Enter Yes or No:').lower().startswith('y')
+if __name__=="__main__":
+    print('Welcome to Tic Tac Toe!')
+    while True:
+        # Reset the board
+        theBoard = [' '] * 10
+        player1_marker, player2_marker = player_input()
+        turn = choose_first()
+        print(turn + ' will go first.')
+        game_on = True
+
+        while game_on:
+            if turn == 'Player 1':
+                # Player1's turn.
+                
+                display_board(theBoard)
+                position = player_choice(theBoard)
+                place_marker(theBoard, player1_marker, position)
+
+                if win_check(theBoard, player1_marker):
+                    display_board(theBoard)
+                    print('Congratulations! You have won the game!')
+                    game_on = False
+                else:
+                    if full_board_check(theBoard):
+                        display_board(theBoard)
+                        print('The game is a draw!')
+                        break
+                    else:
+                        turn = 'Player 2'
+
+            else:
+                # Player2's turn.
+                
+                display_board(theBoard)
+                position = player_choice(theBoard)
+                place_marker(theBoard, player2_marker, position)
+
+                if win_check(theBoard, player2_marker):
+                    display_board(theBoard)
+                    print('Player 2 has won!')
+                    game_on = False
+                else:
+                    if full_board_check(theBoard):
+                        display_board(theBoard)
+                        print('The game is a tie!')
+                        break
+                    else:
+                        turn = 'Player 1'
+
+        if not replay():
+            break                                                                         
